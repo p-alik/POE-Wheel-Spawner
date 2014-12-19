c89654331e281974bf5eede2c94d4c56889a1d9f
Alexei Pastuchov <alexei.pastuchov@telecolumbus.de>
2014-12-19 17:08:14 +0100

    initial commit

---
+++ lib/Poe/Wheel/Spawner.pm
@@ -0,0 +1,81 @@
+package Poe::Wheel::Spawner;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Poe::Wheel::Spawner
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+TBD
+
+=head1 SUBROUTINES/METHODS
+
+
+=head1 AUTHOR
+
+Alexei Pastuchov E<lt>palik at cpan.orgE<gt>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-poe-wheel-spawner at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Poe-Wheel-Spawner>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Poe::Wheel::Spawner
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Poe-Wheel-Spawner>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Poe-Wheel-Spawner>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Poe-Wheel-Spawner>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Poe-Wheel-Spawner/>
+
+=back
+
+
+=head1 REPOSITORY
+
+L<https://github.com/p-alik/Poe-Wheel-Spawner.git>
+
+=head1 LICENSE AND COPYRIGHT
+
+
+Copyright 2014 by Alexei Pastuchov E<lt>palik at cpan.orgE<gt>.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1; # End of Poe::Wheel::Spawner
---
+++ t/00-load.t
@@ -0,0 +1,13 @@
+#!perl -T
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+use Test::More;
+
+plan tests => 1;
+
+BEGIN {
+    use_ok( 'Poe::Wheel::Spawner' ) || print "Bail out!\n";
+}
+
+diag( "Testing Poe::Wheel::Spawner $Poe::Wheel::Spawner::VERSION, Perl $], $^X" );
